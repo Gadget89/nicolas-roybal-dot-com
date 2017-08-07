0d94b0ae066eeb18baae809ba2487a20182433af
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-08-06 23:59:19 -0500

    Inital project migration into deployment repo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// TODO: Un-comment out line 5 and 9 in production build to enable serviceWorker
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
