3741ea3dced2580b605604a94284b4057e11070e
Nicolas Roybal <nicolas.roybal@hotmail.com>
2020-07-01 23:05:42 -0500

    Removed Medium blog links from site.

--- src/Footer.js
+++ src/Footer.js
@@ -1,67 +1,53 @@
-import React, { Component } from 'react';
-import './main.css';
+import React, { Component } from "react";
+import "./main.css";
 
 const footerItem = [
   {
     id: "github",
     link: "https://github.com/Gadget89",
     icon: "fa fa-github fa-3x",
-    title: "Check out my GitHub."
+    title: "Check out my GitHub.",
   },
   {
     id: "linkedin",
     link: "https://www.linkedin.com/in/nicolas-roybal-54bb1413a",
     icon: "fa fa-linkedin-square fa-3x",
-    title: "Check out my LinkedIn."
-  },
-  {
-    id: "medium",
-    link: "https://medium.com/@nicolas.roybal/",
-    icon: "fa fa-medium fa-3x",
-    title: "Check out my blog on Medium."
+    title: "Check out my LinkedIn.",
   },
   {
     id: "codepen",
     link: "http://codepen.io/nickr999/",
     icon: "fa fa-codepen fa-3x",
-    title: "Check out my Codepen."
-  }
-]
+    title: "Check out my Codepen.",
+  },
+];
 // Map through array and return a <li>
-const listItems = footerItem.map((item) =>
-  <li
-    key={item.id}>
+const listItems = footerItem.map((item) => (
+  <li key={item.id}>
     <a
       title={item.title}
       href={item.link}
       rel="noopener noreferrer"
-      target="_blank">
+      target="_blank"
+    >
       <i className={item.icon}></i>
     </a>
   </li>
-)
+));
 class Footer extends Component {
-
   render() {
     return (
       <footer className="footer-container">
+        <p className="footer-info">Links</p>
+        <ul className="footer-link-container">{listItems}</ul>
+        <p className="footer-info">2017 All Rights Reserved.</p>
         <p className="footer-info">
-          Links
+          Made with <i className="fa fa-heart" aria-hidden="true"></i> in San
+          Antonio, TX.
         </p>
-        <ul className="footer-link-container">
-          {listItems}
-        </ul>
-        <p className="footer-info">
-          2017 All Rights Reserved.
-        </p>
-        <p className="footer-info">
-          Made with <i className="fa fa-heart" aria-hidden="true"></i> in San Antonio, TX.
-        </p>
-
       </footer>
     );
   }
 }
-
 
 export default Footer;
--- src/Nav.js
+++ src/Nav.js
@@ -1,124 +1,100 @@
-import React, { Component } from 'react';
-import Headroom from 'react-headroom'
-import './main.css';
-import Logo from './assets/new-logo.png';
-
+import React, { Component } from "react";
+import Headroom from "react-headroom";
+import "./main.css";
+import Logo from "./assets/new-logo.png";
 
 // Array of navigation buttons data
 const navButtons = [
-
   {
     id: "about",
     name: "my story",
     link: "#about",
-    title: "Check out my story."
+    title: "Check out my story.",
   },
   {
     id: "skills",
     name: "skills",
     link: "#skills",
-    title: "Check out my skills."
+    title: "Check out my skills.",
   },
   {
     id: "projects",
     name: "projects",
     link: "#projects",
-    title: "Check out my projects"
+    title: "Check out my projects",
   },
   {
     id: "contact",
     name: "contact me",
     link: "#contact",
-    title: "Let's get in touch"
+    title: "Let's get in touch",
   },
-  {
-    id: "blog",
-    link: "https://medium.com/@nicolas.roybal",
-    name: "blog",
-    title: "Check out my blog"
-  }
-]
-
+];
 
 // Map through array and return a <li>
-const listItems = navButtons.map((button) =>
-  <li
-    key={button.id}>
-    <a
-      title={button.title}
-      href={button.link}
-      onClick={closeNav}>
+const listItems = navButtons.map((button) => (
+  <li key={button.id}>
+    <a title={button.title} href={button.link} onClick={closeNav}>
       {button.name}
     </a>
   </li>
-)
+));
 
-function  openNav() {
-  document.getElementById('modal').className = "open-mobile-nav"
-  document.getElementById('open').style.display = "none"
-  document.getElementById('exit').style.display = "block"
+function openNav() {
+  document.getElementById("modal").className = "open-mobile-nav";
+  document.getElementById("open").style.display = "none";
+  document.getElementById("exit").style.display = "block";
   // document.getElementById('open').className = "close"
   // document.getElementById('exit').className = "open"
-  document.getElementById('exit').style.display = "block"
-  window.scrollTo(0, 0)
+  document.getElementById("exit").style.display = "block";
+  window.scrollTo(0, 0);
 }
 
 function closeNav() {
-  document.getElementById('modal').className = "close-mobile-nav"
-  document.getElementById('exit').style.display = "none"
-  document.getElementById('open').style.display = "block"
+  document.getElementById("modal").className = "close-mobile-nav";
+  document.getElementById("exit").style.display = "none";
+  document.getElementById("open").style.display = "block";
   // document.getElementById('exit').className = "close"
   // document.getElementById('open').className = "open"
-  document.getElementById('open').style.display = "block"
+  document.getElementById("open").style.display = "block";
 }
 
 class Nav extends Component {
-
-
   render() {
     return (
       <section>
-        <Headroom style={{zIndex: 50}}>
-          <nav
-            className="nav-container">
-            <ul
-              className="nav-list-container"
-              id="mobile-disp-hide">
+        <Headroom style={{ zIndex: 50 }}>
+          <nav className="nav-container">
+            <ul className="nav-list-container" id="mobile-disp-hide">
               {listItems}
             </ul>
-            <a
-              href="">
+            <a href="">
               <img
                 alt=""
-                title='Go back to home page'
-                className='nav-logo'
-                src={Logo}>
-              </img>
+                title="Go back to home page"
+                className="nav-logo"
+                src={Logo}
+              ></img>
             </a>
-            <span
-              className="mobile-nav-menu-icon">
-                <div>
-                  <i
-                    id="open"
-                    className="open fa fa-bars fa-2x"
-                    onClick={openNav}
-                    aria-hidden="true">
-                  </i>
-                  <i
-                    id="exit"
-                    className="close fa fa-times fa-2x"
-                    onClick={closeNav}
-                    aria-hidden="true">
-                  </i>
-                </div>
+            <span className="mobile-nav-menu-icon">
+              <div>
+                <i
+                  id="open"
+                  className="open fa fa-bars fa-2x"
+                  onClick={openNav}
+                  aria-hidden="true"
+                ></i>
+                <i
+                  id="exit"
+                  className="close fa fa-times fa-2x"
+                  onClick={closeNav}
+                  aria-hidden="true"
+                ></i>
+              </div>
             </span>
-            <div
-              className='nav-background'>
-            </div>
+            <div className="nav-background"></div>
           </nav>
-          <ul
-            className="nav-modal"
-            id="modal">
+          <ul className="nav-modal" id="modal">
             {listItems}
           </ul>
         </Headroom>
@@ -126,6 +102,5 @@ class Nav extends Component {
     );
   }
 }
-
 
 export default Nav;
